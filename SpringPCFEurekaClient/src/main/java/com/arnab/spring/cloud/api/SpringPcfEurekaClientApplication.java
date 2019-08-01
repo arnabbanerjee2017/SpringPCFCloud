eeb5017f763730ff1699590c9407a88dcb62243e
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-01 22:11:27 +0530

    Added Eureka Client

---
+++ SpringPCFEurekaClient/src/main/java/com/arnab/spring/cloud/api/SpringPcfEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPcfEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPcfEurekaClientApplication.class, args);
+	}
+
+}
