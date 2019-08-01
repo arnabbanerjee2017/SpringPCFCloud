93dafca7dfe93ba35867121f691bc30a168013a8
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-01 22:36:33 +0530

    Changes for Eureka Client.

--- SpringPCFEurekaClient/src/main/java/com/arnab/spring/cloud/api/SpringPcfEurekaClientApplication.java
+++ SpringPCFEurekaClient/src/main/java/com/arnab/spring/cloud/api/SpringPcfEurekaClientApplication.java
@@ -2,8 +2,10 @@ package com.arnab.spring.cloud.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringPcfEurekaClientApplication {
 
 	public static void main(String[] args) {
