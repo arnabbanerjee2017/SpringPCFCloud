dc4a236518f60188061391e74f0e2f4d5a30087e
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-01 22:30:32 +0530

    Added @EnableEurekaServer

--- SpringPCFEurekaServer/src/main/java/com/arnab/spring/cloud/api/SpringPcfEurekaServerApplication.java
+++ SpringPCFEurekaServer/src/main/java/com/arnab/spring/cloud/api/SpringPcfEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.arnab.spring.cloud.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringPcfEurekaServerApplication {
 
 	public static void main(String[] args) {
