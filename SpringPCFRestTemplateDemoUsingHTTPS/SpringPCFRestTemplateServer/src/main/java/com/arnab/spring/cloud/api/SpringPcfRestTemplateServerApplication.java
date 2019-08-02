14527e7f52f19cd511e4be8515cbd56f5aff3c0c
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-02 15:22:03 +0530

    Added RestTemplate Demo using HTTPS

---
+++ SpringPCFRestTemplateDemoUsingHTTPS/SpringPCFRestTemplateServer/src/main/java/com/arnab/spring/cloud/api/SpringPcfRestTemplateServerApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPcfRestTemplateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPcfRestTemplateServerApplication.class, args);
+	}
+
+}
